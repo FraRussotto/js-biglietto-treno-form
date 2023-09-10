e249b6ecf1a7f22f630fc5ee5888980ebfd6b68d
FraRussotto <russottofrancesco1@gmail.com>
2023-09-10 22:00:18 +0200

    [ADD] QuerySelector

--- js/script.js
+++ js/script.js
@@ -9,19 +9,34 @@ let infoUser = "Nome e cognome"
 let distance = "0";
 let age = "0";
 let prom = "offer";
-let carrozza = "0";
+let nCarriage = "0";
 let postalCode = "00000";
 let price = "€"
 let insertOk = true;
+let message;
 
 
 btnInsert.addEventListener("click", function(){
-  infoUser = document.getElementById("name").value;
+  nameUser = document.getElementById("name").value;
   distance = parseInt(document.getElementById("km").value);
-  age = parseInt(document.getElementById("age").value);
+  ageUser = parseInt(document.getElementById("age").value);
+  
 
+  price = distance * km_price;
+  document.querySelector(".discount").innerHTML = "No promo";
 
-console.log(infoUser);
-console.log(distance);
-console.log(age);
+  if(ageUser == 1){
+    price *= 1 - (discount20/100);
+    document.querySelector(".discount").innerHTML = "Promo YOUNG";
+    document.querySelector(".final_price").innerHTML = price;
+  }else if(ageUser == 3){
+    price *= 1 - (discount40/100);
+    document.querySelector(".discount").innerHTML = "Promo OVER 65";
+    document.querySelector(".final_price").innerHTML = price;
+  }
+  
+  document.querySelector(".final_price").innerHTML = price;
+  document.querySelector(".user").innerHTML = nameUser;
+  document.querySelector(".n_carriage").innerHTML = nCarriage;
+  document.querySelector(".cap").innerHTML = postalCode;
 });
